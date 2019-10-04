8895808249ad2aa3c5dbbfe24cf78595d7499a4b
Simon Arnell <simon.arnell@gmail.com>
2019-10-04 22:02:45 +0100

    added slideshow logic

--- assets/js/edvininpolku.js
+++ assets/js/edvininpolku.js
@@ -1,3 +1,6 @@
+var blobs = [];
+var count = 0;
+
 fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images')
   .then((response) => {
     console.debug(response)
@@ -6,7 +9,8 @@ fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images
       if(Array.isArray(data)) {
         data.map(file => {
           fetch(file.download_url)
-            .then((dlres) => console.debug(dlres))
+            .then((dlres) => dlres.blob())
+            .then((blob) => blobs.push(blob))
             .catch((err) => console.error('error fetching imag :-S', err))
         })
       }
@@ -15,3 +19,19 @@ fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images
   .catch((err) => {
     console.log('error fetching images list :-S', err)
   })
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    setInterval(() => {
+      if(blobs.length > 0) {
+        var canvas = document.getElementById('canvas');
+        var img = new Image();
+        img.onload = () => {
+          var ctx = canvas.getContext('2d')
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+        }
+        img.src = URL.createObjectURL(blobs[count]);
+        count = (count + 1) % blobs.length;
+      }
+    }, 2000)
+  });
