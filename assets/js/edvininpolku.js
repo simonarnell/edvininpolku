7b7e7da7073e86c5d5bbb10ccb1e3db170acc204
Simon Arnell <simon.arnell@gmail.com>
2019-10-04 20:41:40 +0100

    moved to debug

--- assets/js/edvininpolku.js
+++ assets/js/edvininpolku.js
@@ -1,6 +1,6 @@
 fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images')
   .then((response) => {
-    console.log(response)
+    console.debug(response)
   })
   .catch((err) => {
     console.log('error fetching images list :-S', err)
