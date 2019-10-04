93e10d69a701efe258fe909728d6405f16485b94
Simon Arnell <simon.arnell@gmail.com>
2019-10-04 20:42:33 +0100

    parsing images json structure

--- assets/js/edvininpolku.js
+++ assets/js/edvininpolku.js
@@ -1,6 +1,9 @@
 fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images')
   .then((response) => {
     console.debug(response)
+    response.json().then((data) => {
+      console.debug(data)
+    })
   })
   .catch((err) => {
     console.log('error fetching images list :-S', err)
