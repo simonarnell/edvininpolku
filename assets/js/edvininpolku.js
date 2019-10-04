ee121c853d4442bd6ab82c2c3d40fe08c16f3987
Simon Arnell <simon.arnell@gmail.com>
2019-10-04 22:57:09 +0100

    correcting whitespace

--- assets/js/edvininpolku.js
+++ assets/js/edvininpolku.js
@@ -20,18 +20,18 @@ fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images
     console.log('error fetching images list :-S', err)
   })
 
-  document.addEventListener("DOMContentLoaded", function(event) {
-    setInterval(() => {
-      if(blobs.length > 0) {
-        var canvas = document.getElementById('canvas');
-        var img = new Image();
-        img.onload = () => {
-          var ctx = canvas.getContext('2d')
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-        }
-        img.src = URL.createObjectURL(blobs[count]);
-        count = (count + 1) % blobs.length;
+document.addEventListener("DOMContentLoaded", function(event) {
+  setInterval(() => {
+    if(blobs.length > 0) {
+      var canvas = document.getElementById('canvas');
+      var img = new Image();
+      img.onload = () => {
+        var ctx = canvas.getContext('2d')
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
       }
-    }, 2000)
-  });
+      img.src = URL.createObjectURL(blobs[count]);
+      count = (count + 1) % blobs.length;
+    }
+  }, 2000)
+});
