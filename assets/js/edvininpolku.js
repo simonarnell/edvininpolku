c6a2778fe0e6946a4effc9b05088a7db862308f5
Simon Arnell <simon.arnell@gmail.com>
2019-10-05 19:41:54 +0100

    added map

--- assets/js/edvininpolku.js
+++ assets/js/edvininpolku.js
@@ -70,3 +70,15 @@ Promise.resolve(document.ready)
       }
     }, 2000)
   })
+
+  Promise.all([document.ready, configured])
+    .then(() => {
+      var map = L.map('map');
+      map.setView([51, 0], 13);
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: config.apikeys.mapbox
+      }).addTo(map);
+    })
