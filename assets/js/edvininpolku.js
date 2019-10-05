d556501bc44a4263d16c8778f5e4a1bc17f0ab93
Simon Arnell <simon.arnell@gmail.com>
2019-10-05 19:31:33 +0100

    refactored to use external config file

--- assets/js/edvininpolku.js
+++ assets/js/edvininpolku.js
@@ -2,44 +2,54 @@ var blobs = [];
 var points = []
 var count = 0;
 var featureCollection
+var config
 
-fetch('https://api.github.com/repos/simonarnell/edvininpolku/contents?ref=images')
-  .then((response) => {
-    console.debug(response)
-    response.json().then((data) => {
-      console.debug(data)
-      if(Array.isArray(data)) {
-        Promise.all(data.map(file => {
-          return new Promise((resolve) => {
-            fetch(file.download_url)
-              .then((response) => response.blob())
-              .then((blob) => {
-                blobs.push(blob)
-                var fileReader = new FileReader();
-                return new Promise((resolve) => {
-                  fileReader.onload = (event) => {
-                    var buffer = event.target.result;
-                    var webworker = new Worker('assets/js/exif-webworker.js');
-                    webworker.onmessage = (event) => resolve(JSON.parse(event.data))
-                    webworker.postMessage(buffer, [buffer])
-                  }
-                  fileReader.readAsArrayBuffer(blob);
+var configured = fetch('assets/data/config.json')
+  .then(response => response.json())
+  .then(json => {
+    console.debug(json)
+    config = json
+  })
+
+Promise.resolve(configured).then(() => {
+  fetch(config.urls.imagesBranch)
+    .then((response) => {
+      console.debug(response)
+      response.json().then((data) => {
+        console.debug(data)
+        if(Array.isArray(data)) {
+          Promise.all(data.map(file => {
+            return new Promise((resolve) => {
+              fetch(file.download_url)
+                .then((response) => response.blob())
+                .then((blob) => {
+                  blobs.push(blob)
+                  var fileReader = new FileReader();
+                  return new Promise((resolve) => {
+                    fileReader.onload = (event) => {
+                      var buffer = event.target.result;
+                      var webworker = new Worker('assets/js/exif-webworker.js');
+                      webworker.onmessage = (event) => resolve(JSON.parse(event.data))
+                      webworker.postMessage(buffer, [buffer])
+                    }
+                    fileReader.readAsArrayBuffer(blob);
+                  })
                 })
+                .then(exif => resolve(exif))
+                .catch((err) => console.error('error fetching image :-S', err))
               })
-              .then(exif => resolve(exif))
-              .catch((err) => console.error('error fetching image :-S', err))
-            })
-      }))
-      .then((points) => {
-          console.debug(points)
-          featureCollection = {
-            "type": "FeatureCollection",
-            "properties": {},
-            "features": points.map(point => point)
-          }
-          console.log(featureCollection)
-      })
-    }
+        }))
+        .then((points) => {
+            console.debug(points)
+            featureCollection = {
+              "type": "FeatureCollection",
+              "properties": {},
+              "features": points.map(point => point)
+            }
+            console.log(featureCollection)
+        })
+      }
+    })
   })
 })
 document.ready = new Promise(
